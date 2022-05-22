ced6bcfbc13e1f2ffad4b35bafdcd854a02c0720
babuece97 <babuece97@yahoo.com.com>
2022-05-22 17:03:13 +0200

    404 pic uploaded

--- src/compoments/NotFound/NotFound.js
+++ src/compoments/NotFound/NotFound.js
@@ -3,7 +3,15 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            Sorry!!! 404, wrong path
+           
+            <div> 
+            <img className='h-96 w-full object-cover object-center'  src="https://img.youtube.com/vi/Zw_FKq10S8M/maxresdefault.jpg" alt='' />
+            </div>
+                    
+                    
+                 
+                      
+                  
         </div>
     );
 };
